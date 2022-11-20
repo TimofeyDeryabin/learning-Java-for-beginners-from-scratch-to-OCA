81823e93f027e99edcf25b79c806bec199bea71f
Timofey Deryabin <timofeyderyabin86@gmail.com>
2022-11-20 11:23:55 +0500

    Lesson21

---
+++ src/lesson21/Test7.java
@@ -0,0 +1,21 @@
+package lesson21;
+
+public class Test7 {
+    int a = 5;
+    int l;
+    int t, h = 5, k;
+
+    public void abc(int a, int b) {
+        int c;
+        c = 10;
+        System.out.println("privet");
+    }
+    public void abc(double a, double b) {
+        System.out.println("poka");
+    }
+
+    public static void main(String[] args) {
+        Test7 t = new Test7();
+        t.abc(1, 2);
+    }
+}
