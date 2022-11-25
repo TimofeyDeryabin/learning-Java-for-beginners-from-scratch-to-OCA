fb2e86b72d70fafc409efd21910a2e24c2fa2d32
Timofey Deryabin <timofeyderyabin86@gmail.com>
2022-11-25 15:53:05 +0500

    Lesson26

---
+++ src/lesson26/Test2.java
@@ -0,0 +1,42 @@
+package lesson26;
+
+import java.util.ArrayList;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);
+        int a = list.get(0);
+        System.out.println(a);
+//        Integer b = new Integer(10);
+//        int c = b;
+//        switch(b) {
+//
+//        }
+
+//        Number d = new Integer(20);
+//        c = (Integer) d;
+
+        Long l = 50l;
+
+        String s1 = "50";
+        String s2 = "ajfa'true";
+        String s3 = "3.14";
+        int i1 = Integer.parseInt(s1);
+        boolean b1 = Boolean.parseBoolean(s2);
+        double d = Double.parseDouble(s3);
+        System.out.println(i1);
+        System.out.println(b1);
+        System.out.println(d);
+
+        Integer i3 = Integer.valueOf(10);
+        System.out.println(i3);
+
+        Double d2 = Double.valueOf(s3);
+        System.out.println(s3);
+
+        Byte b10 = Byte.valueOf((byte) 10);
+        Integer i6 = Integer.valueOf(6);
+        System.out.println(b10.equals(d2));
+    }
+}
---
+++ src/lesson26/Test7.java
@@ -0,0 +1,49 @@
+package lesson26;
+
+public class Test7 {
+    public static void main(String[] args) {
+        Lion l = new Lion();
+    }
+}
+
+class Animal {
+    Animal() {
+        System.out.println("Construktor of Animal");
+    }
+
+    static {
+        System.out.println("static init in Animal");
+    }
+
+    {
+        System.out.println("non-static init in Animal");
+    }
+}
+
+class Mammal extends Animal {
+    Mammal() {
+        System.out.println("Construktor of Mammal");
+    }
+
+    static {
+        System.out.println("static init in Mammal");
+    }
+
+    {
+        System.out.println("non-static init in Mammal");
+    }
+}
+
+class Lion extends Mammal {
+    Lion() {
+        System.out.println("Construktor of Lion");
+    }
+
+    static {
+        System.out.println("static init in Lion");
+    }
+
+    {
+        System.out.println("non-static init in Lion");
+    }
+}
